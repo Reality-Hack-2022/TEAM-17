de7e3d73325d8ed6374221f126dfaf1cabae8f6e
NemeanLion42 <59586599+NemeanLion42@users.noreply.github.com>
2022-03-25 22:10:23 -0400

    Use Oculus Voice SDK to get voice input and send to GPT3

--- The-Imagination-Machine/Assets/Scripts/GPT3Interface.cs
+++ The-Imagination-Machine/Assets/Scripts/GPT3Interface.cs
@@ -11,35 +11,24 @@ using TMPro;
 public class GPT3Interface : MonoBehaviour
 {
     public string story;
-    OpenAIAPI api;
-    public GameObject inputObject;
-    public string text;
-    string learningData;
-    int maxTokens;
+    private OpenAIAPI api;
+    public TextMeshProUGUI ErrorLog;
+    private string learningData;
+    private int maxTokens;
+    private int requestsMade;
+    public TextMeshProUGUI RequestCounter;
     // Start is called before the first frame update
     void Start() {
+        requestsMade = 0;
         maxTokens = 500;
         learningData = "Write a short love story:\nHer soul was a dandelion, carried to all corners of the earth by the wind, scattering into the clouds beyond. Sometimes, when she’s out here alone, she can feel the pulse of something bigger, as if all things animate were beating in unison, a glory and a connection that sweeps her out of herself, out of her consciousness, so that nothing has a name, not in Latin, not in English, not in any known language. All around her, in the sunlight, snow melts, crystals evaporate into a steam, into nothing. In the firelight, fragile things burst and disappear.\nAnd so we rushed down toward the river together, dancing through the trees like the entire world was but a stage for our madness. For the only people for me are the mad ones, the ones who are mad to live, mad to talk, mad to be saved, desirous of everything at the same time, the ones who never yawn or say a commonplace thing, but burn, burn, burn like fabulous yellow roman candles exploding like spiders across the stars! For we are the ones will not be stopped, and we'll go on and on and on, until we've consumed everything and are left in the ashes.\n###\nWrite a short science fiction story:\nCyberspace. A consensual hallucination experienced daily by billions of legitimate operators, in every nation, by children being taught mathematical concepts... A graphic representation of data abstracted from the banks of every computer in the human system. Unthinkable complexity. Lines of light ranged in the nonspace of the mind, clusters and constellations of data. Like city lights, receding.\nA year here and he still dreamed of cyberspace, hope fading nightly. All the speed he took, all the turns he'd taken and the corners he cut in Night City, and he'd still see the matrix in his dreams, bright lattices of logic unfolding across that colourless void... The Sprawl was a long, strange way home now over the Pacific, and he was no Console Man, no cyberspace cowboy. Just another hustler, trying to make it through. But the dreams came on in the Japanese night like livewire voodoo, and he'd cry for it, cry in his sleep, and wake alone in the dark, curled in his capsule in some coffin hotel, hands clawed into the bedslab, temper foam bunched between his fingers, trying to reach the console that wasn't there.\nHis vision crawled with ghost hieroglyphs, translucent lines of symbols arranging themselves against the neutral backdrop of the bunker wall. He looked at the backs of his hands, saw faint neon molecules crawling beneath the skin, ordered by the unknowable code. He raised his right hand and moved it experimentally. It left a faint, fading trail of strobed afterimages.\n###\n";
-        // story = "/*Once upon a time there was a king.\nHe was a bad king.\nNo one liked him.\n";
         story = "Write a short story:\n";
-        // story = "";
-        // Task task = GenerateMore();
-
-        var keypath = Path.Combine(Application.streamingAssetsPath, "apikey.txt");
-        if (File.Exists(keypath) == false)
-        {
-            Debug.LogError("API key missing: " + keypath + "\nPlease add the API key");
-        }
-
-        //Debug.Log("Load apikey: " + keypath);
-        string apikey = File.ReadAllText(keypath);
-        api = new OpenAIAPI(apikey, Engine.Davinci);
+        // API key must be removed before pushing to GitHub, so it should only be present for building
+        api = new OpenAIAPI("", Engine.Davinci);
+        
     }
 
-    public void GenerateText() {
-        GenerateMore();
-    }
-    public async Task GenerateMore()
+    private async Task GenerateMore()
     {
         try {
             string prompt = learningData + story;
@@ -55,28 +44,29 @@ public class GPT3Interface : MonoBehaviour
             story += (await api.Completions.CreateCompletionAsync(new CompletionRequest(
                 prompt: prompt,
                 stopSequences: "\n",
-                temperature: 0.8,
+                temperature: 0.5,
                 max_tokens: maxTokens
             ))).ToString() + "\n";
+            requestsMade++;
             Debug.Log("AI\n" + story);
         } catch (System.Exception e) {
             Debug.LogError(e.Message);
+            ErrorLog.text += e.Message + "\n" + e.StackTrace + "\n";
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        RequestCounter.text = requestsMade.ToString();
         string[] lines = story.Split("\n".ToCharArray());
         string toShow = lines[Mathf.Max(lines.Length-2, 1)].Trim() + "";
         gameObject.GetComponent<TextMeshPro>().text = toShow;
-        // TextMeshProUGUI newInput = inputObject.GetComponent<TextMeshProUGUI>();
-        TMP_InputField newInput = inputObject.GetComponent<TMP_InputField>();
-        if (Input.GetKeyUp(KeyCode.Return)) {
-            story += newInput.text;
-            newInput.text = "";
-            Debug.Log("User\n" + story);
-            GenerateMore();
-        }
+    }
+
+    public void SendInput(string input) {
+        story += input;
+        Debug.Log("User\n" + story);
+        GenerateMore();
     }
 }
